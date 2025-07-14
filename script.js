d96e597b2e5fdfffd4955978163f2d317e599a50
Marcos Nicolau <marcosnicolauoliveira@outlook.com>
2025-07-14 10:01:51 -0300

    feat(JS): key handle verification added
    - verify if the user inputs is a letter
    - add auto tab and auto delete focus

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 const inputWordle = document.querySelector(".wordle-game");
 
-inputWordle.addEventListener("input", () => {
-    console.log("Alguem digitou algo;")
-})
+
+inputWordle.addEventListener("input", (event) => {
+    const char = event.target.value;
+    const parentDiv = event.target.parentElement;
+    const inputs = Array.from(parentDiv.querySelectorAll("input"))
+    const currentIndex = inputs.indexOf(event.target);
+
+    if (char.length === 1 && char.match(/[a-zA-Z]/)) {
+        if (currentIndex < inputs.length - 1) {
+            inputs[currentIndex + 1].focus();
+        }
+        if (currentIndex === inputs.length - 1) {
+            console.log("Fim linha");
+        }
+    
+    } else {
+        event.target.value = "";
+    }
+});
+
+inputWordle.addEventListener("keydown", (event) => {
+    if (event.key === "Backspace") {
+        const parentDiv = event.target.parentElement;
+        const inputs = Array.from(parentDiv.querySelectorAll("input"))
+        const currentIndex = inputs.indexOf(event.target);
+        if (event.target.value === "" && currentIndex > 0) {
+            inputs[currentIndex-1].focus();
+        }
+    }
+});
